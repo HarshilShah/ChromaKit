1ae3c16cdce33bc9d4b34b8a0deb4f896bbe37f3
Harshil Shah <harshilshah1910@outlook.com>
2024-01-14 18:42:54 +0530

    Added some basic conversion tests

--- Tests/ChromaKitTests/ChromaKitTests.swift
+++ Tests/ChromaKitTests/ChromaKitTests.swift
@@ -1,12 +1,40 @@
 import XCTest
 @testable import ChromaKit
 
-final class ChromaKitTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+func XCTAssertEqualP3(
+	_ a: P3,
+	_ b: P3,
+	accuracy: Double = 0.001,
+	file: StaticString = #filePath,
+	line: UInt = #line
+) {
+	XCTAssertEqual(a.r, b.r, accuracy: accuracy, file: file, line: line)
+	XCTAssertEqual(a.g, b.g, accuracy: accuracy, file: file, line: line)
+	XCTAssertEqual(a.b, b.b, accuracy: accuracy, file: file, line: line)
+}
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+final class ChromaKitTests: XCTestCase {
+	let nativeP3 = P3(r: 0.8, g: 0.6, b: 0.4)
+	
+    func testLch() throws {
+		let lch = Lch(l: 67.21298, c: 43.18819, h: 66.09192)
+		XCTAssertEqualP3(lch.p3(), nativeP3)
+    }
+	
+	func testOklch() throws {
+		let oklch = Oklch(l: 0.72385, c: 0.10601, h: 62.26385)
+		print(oklch.xyz())
+		XCTAssertEqualP3(oklch.p3(), nativeP3)
+	}
+	
+    func testLab() throws {
+		let lab = Lab(l: 67.21298, a: 17.5029, b: 39.48251)
+		XCTAssertEqualP3(lab.p3(), nativeP3)
+    }
+	
+    func testOklab() throws {
+		let oklab = Oklab(l: 0.72385, a: 0.04934, b: 0.09383)
+		print(oklab.xyz())
+		XCTAssertEqualP3(oklab.p3(), nativeP3)
     }
 }
